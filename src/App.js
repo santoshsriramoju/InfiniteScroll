e4576c49716a7b926e0eef8e79d8a15b4c237de5
santoshsriramoju <santoshsriramoju2691@gmail.com>
2023-01-03 14:56:41 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useCallback } from "react";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { loading, error, hasMore, books } = useBookSearch(query, pageNumber);
+
+
+  const observer = useRef();
+  const lastBookElementRef= useCallback(node=>{
+    //While loading you shouldn't make calls, else runs into infinite loop
+    if(loading) return;
+    //Disconnect the previous observer
+      if(observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(entries =>{
+        //only have one node, the last element
+        //isIntersecting tells whether the node is on the page somewhere
+        if(entries[0].isIntersecting && hasMore){
+          setPageNumber(pageNumber => pageNumber + 1)
+        }
+      })
+      if(node) observer.current.observe(node)
+  },[loading, hasMore])
+
+  const handleInput = e => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" value={query} onChange={handleInput}></input>
+      {
+        books.map((book,index) => {
+          if(books.length === index+1){
+           return <div ref={lastBookElementRef} key={book}>{book}</div>
+          }else{
+           return <div key={book}>{book}</div>
+          }
+        })
+      }
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
+    </>
   );
 }
 
